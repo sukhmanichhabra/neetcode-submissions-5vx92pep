b9e82cd53c33f4718c1a44969241247fa526546b
sukhmani chhabra <149159326+sukhmanichhabra@users.noreply.github.com>
2026-05-26 23:20:12 +0530

    Add: longest-turbulent-subarray - submission-0

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& arr) {
+        int n = arr.size();
+        if (n == 1) return 1;
+
+        vector<vector<int>> dp(n, vector<int>(2, 1));
+        int maxLen = 1;
+
+        for (int i = 1; i < n; ++i) {
+            if (arr[i] > arr[i - 1]) {
+                dp[i][1] = dp[i - 1][0] + 1;
+            } else if (arr[i] < arr[i - 1]) {
+                dp[i][0] = dp[i - 1][1] + 1;
+            }
+            maxLen = max(maxLen, max(dp[i][0], dp[i][1]));
+        }
+
+        return maxLen;
+    }
+};
